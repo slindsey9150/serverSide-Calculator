d3466ad23563bdfd1335aa800a953d11921cb503
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-08 20:27:40 -0600

    every function now modifies the original array instead of adding to a new array

--- doMathPractice.js
+++ doMathPractice.js
@@ -34,27 +34,45 @@ function doMath() {
             let index=practiceArray.indexOf(value);
             practiceArray[index] = updatedValue
             // console.log('this is the results array', resultsArray);
-            console.log("this is the modified practice array", practiceArray);
+            // console.log("this is the modified practice array", practiceArray);
         }
-        // if (value.operator === '-') {
-        //     let finalResult = subtractFunction(value.numOne, value.numTwo);
-        //     // console.log("this is the subtractionfinalResult", finalResult);
-        //     resultsArray.push(value, finalResult);
-        //     // console.log('this is the results array', resultsArray);
-        // }
-        // if (value.operator === '*') {
-        //     let finalResult = multiplyFunction(value.numOne, value.numTwo);
-        //     // console.log("this is the multiplicationfinalResult", finalResult);
-        //     resultsArray.push(value,finalResult);
-        //     // console.log('this is the results array', resultsArray);
-        // }
-        // if (value.operator === '/') {
-        //     let finalResult = divideFunction(value.numOne, value.numTwo);
-        //     // console.log("this is the multiplicationfinalResult", finalResult);
-        //     // Alternatively, you can push them as an array
-        //     resultsArray.push(value, finalResult);
-        //     // console.log('this is the results array', resultsArray);
-        // }
+        if (value.operator === '-') {
+            let result = subtractFunction(value.numOne, value.numTwo);
+            // console.log("this is the subtractionfinalResult", finalResult);
+            // resultsArray.push(value, finalResult);
+            let updatedValue = {...value, result}
+            console.log(result);
+            let index=practiceArray.indexOf(value);
+            practiceArray[index] = updatedValue
+            // console.log('this is the results array', resultsArray);
+            // console.log("this is the modified practice array", practiceArray);
+
+        }
+        if (value.operator === '*') {
+            let result = multiplyFunction(value.numOne, value.numTwo);
+            // console.log("this is the multiplicationfinalResult", finalResult);
+            // resultsArray.push(value,finalResult);
+            let updatedValue = {...value, result}
+            console.log(result);
+            let index=practiceArray.indexOf(value);
+            practiceArray[index] = updatedValue
+            // console.log('this is the results array', resultsArray);
+            // console.log("this is the modified practice array", practiceArray);
+
+        }
+        if (value.operator === '/') {
+            let result = divideFunction(value.numOne, value.numTwo);
+            // console.log("this is the multiplicationfinalResult", finalResult);
+            // Alternatively, you can push them as an array
+            // resultsArray.push(value, finalResult);
+            let updatedValue = {...value, result}
+            console.log(result);
+            let index=practiceArray.indexOf(value);
+            practiceArray[index] = updatedValue
+            // console.log('this is the results array', resultsArray);
+            console.log("this is the modified practice array", practiceArray);
+
+        }
     }
     // console.log("this is the practice array", practiceArray);
     // console.log('this is the results array', resultsArray);
@@ -66,24 +84,24 @@ function addFunction(firstNumber, secondNumber) {
     // console.log('this is the addFunction result', result);
     return result; // Add a return statement here
 }
-// function subtractFunction(firstNumber, secondNumber)  {
-//         // console.log('this is the subtract function');
-//         let result = firstNumber - secondNumber 
-//         // console.log('this is the subtractFunction result', result);
-//         return result;
-//     }
-//     function multiplyFunction(firstNumber, secondNumber)  {
-//         // console.log('this is the multiply function');
-//         let result = firstNumber * secondNumber 
-//         // console.log('this is the multiplyFunction result', result);
-//         return result;
-//     }
-//     function divideFunction(firstNumber, secondNumber)  {
-//         // console.log('this is the divide function');
-//         let result = firstNumber / secondNumber 
-//         // console.log('this is the divideFUnction result', result);
-//         return result;
-//     }
+function subtractFunction(firstNumber, secondNumber)  {
+        // console.log('this is the subtract function');
+        let result = firstNumber - secondNumber 
+        // console.log('this is the subtractFunction result', result);
+        return result;
+    }
+    function multiplyFunction(firstNumber, secondNumber)  {
+        // console.log('this is the multiply function');
+        let result = firstNumber * secondNumber 
+        // console.log('this is the multiplyFunction result', result);
+        return result;
+    }
+    function divideFunction(firstNumber, secondNumber)  {
+        // console.log('this is the divide function');
+        let result = firstNumber / secondNumber 
+        // console.log('this is the divideFUnction result', result);
+        return result;
+    }
 
 
 
