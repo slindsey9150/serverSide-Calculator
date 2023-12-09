158328f753a443d4451bcc2a18874dacf86ff1e4
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-08 22:24:29 -0600

    can almost display list of previous calculation. can currently display the object of the most previous calculation

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,6 +1,6 @@
 console.log('client.js is sourced!');
+//variable to add math operator to calculations array
 let theOperator = ''
-
 
 function handleGETEquation() {
 axios({
@@ -8,6 +8,14 @@ axios({
     url:'/calculations'
 }).then((response) => {
     console.log('success', response.data);
+       //display most recent result
+   document.getElementById("recentResult").innerHTML = JSON.stringify(response.data[response.data.length-1].result);
+     //display list of previous calculations
+     for (let i=0; i < response.data.length; i++){
+        console.log('response data', response.data);
+     document.getElementById("resultHistory").innerHTML = `${JSON.stringify(response.data[i])}`
+     console.log('response Data',  JSON.stringify(response.data[i]))
+     }
 }).catch((error) => {
     console.log('server error', error);
 })
@@ -33,10 +41,14 @@ function handlePOSTEquation(event) {
         console.log('server error', error);
     })
     handleGETEquation()
+    //resets the input sections in form
     document.getElementById("calculator").reset();
+  
+ 
+
+    //
 }
-
-
+//function to add math operator to calculations array
 function hereAValue(event){
   event.preventDefault()
   console.log('I was Clicked');
