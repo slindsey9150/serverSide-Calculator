3b972ce4fe2b64d80c900361654986c5e8b46032
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-08 19:54:09 -0600

    fixed inserting the addFunction into doMath

--- doMathPractice.js
+++ doMathPractice.js
@@ -1,4 +1,4 @@
-let resultsArray = []
+let resultsArray = [];
 let practiceArray = [
     {
       numOne: 3,
@@ -10,26 +10,36 @@ let practiceArray = [
       numTwo: 7,
       operator: '-',
     }
-  ]
+];
 
-function doMath(){
+function doMath() {
     console.log('do math function');
-    for (value of practiceArray)
-    console.log('this is a value:', value);
-     if (value.operator = '+'){
-        let result = addFunction(value.numOne,value.numTwo) 
-     console.log("this is the result",result);
-        resultsArray.push(value.numOne,value.numTwo,value.operator,result)
-        console.log('this is the results array',resultsArray);
-     }
-}
-    doMath()
-
-    function addFunction(firstNumber, secondNumber)  {
-        console.log('this is the add function');
-        let result = firstNumber + secondNumber
-        console.log('this is the addFunction result', result);
+    for (let value of practiceArray) {
+        console.log('this is a value:', value);
+        if (value.operator === '+') {
+            let finalResult = addFunction(value.numOne, value.numTwo);
+            console.log("this is the finalResult", finalResult);
+            resultsArray.push(value.numOne, value.operator, value.numTwo, finalResult);
+            // Alternatively, you can push them as an array
+            // resultsArray.push([value.numOne, value.operator, value.numTwo, result]);
+            console.log('this is the results array', resultsArray);
+        }
     }
+}
+
+function addFunction(firstNumber, secondNumber) {
+    console.log('this is the add function');
+    let result = firstNumber + secondNumber;
+    console.log('this is the addFunction result', result);
+    return result; // Add a return statement here
+}
+
+doMath();
+
+
+
+
+    
     // function subtractFunction(firstNumber, secondNumber)  {
     //     console.log('this is the add function');
     //     let result = firstNumber - secondNumber 
