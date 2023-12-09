7b18d36602b7f2e361fb4aa6fd460d89b42977ee
slindsey9150 <stevie.lindsey.jr@gmail.com>
2023-12-08 20:02:29 -0600

    doMath now loops through all objects in the array, and applies the function that applies to the specific operator

--- doMathPractice.js
+++ doMathPractice.js
@@ -9,54 +9,82 @@ let practiceArray = [
       numOne: 11,
       numTwo: 7,
       operator: '-',
-    }
+    },
+    {
+        numOne: 3,
+        numTwo: 5,
+        operator: '*',
+      },
+      {
+        numOne: 12,
+        numTwo: 6,
+        operator: '/',
+      }
 ];
-
 function doMath() {
     console.log('do math function');
     for (let value of practiceArray) {
         console.log('this is a value:', value);
         if (value.operator === '+') {
             let finalResult = addFunction(value.numOne, value.numTwo);
-            console.log("this is the finalResult", finalResult);
-            resultsArray.push(value.numOne, value.operator, value.numTwo, finalResult);
+            console.log("this is the additionfinalResult", finalResult);
+            resultsArray.push(value, finalResult);
+            // Alternatively, you can push them as an array
+            // resultsArray.push([value.numOne, value.operator, value.numTwo, result]);
+            console.log('this is the results array', resultsArray);
+        }
+        if (value.operator === '-') {
+            let finalResult = subtractFunction(value.numOne, value.numTwo);
+            console.log("this is the subtractionfinalResult", finalResult);
+            resultsArray.push(value, finalResult);
+            // Alternatively, you can push them as an array
+            // resultsArray.push([value.numOne, value.operator, value.numTwo, result]);
+            console.log('this is the results array', resultsArray);
+        }
+        if (value.operator === '*') {
+            let finalResult = multiplyFunction(value.numOne, value.numTwo);
+            console.log("this is the multiplicationfinalResult", finalResult);
+            resultsArray.push(value,finalResult);
+            // Alternatively, you can push them as an array
+            // resultsArray.push([value.numOne, value.operator, value.numTwo, result]);
+            console.log('this is the results array', resultsArray);
+        }
+        if (value.operator === '/') {
+            let finalResult = divideFunction(value.numOne, value.numTwo);
+            console.log("this is the multiplicationfinalResult", finalResult);
+            resultsArray.push(value,finalResult);
             // Alternatively, you can push them as an array
             // resultsArray.push([value.numOne, value.operator, value.numTwo, result]);
             console.log('this is the results array', resultsArray);
         }
     }
 }
-
 function addFunction(firstNumber, secondNumber) {
     console.log('this is the add function');
     let result = firstNumber + secondNumber;
     console.log('this is the addFunction result', result);
     return result; // Add a return statement here
 }
-
-doMath();
-
-
-
-
-    
-    // function subtractFunction(firstNumber, secondNumber)  {
-    //     console.log('this is the add function');
-    //     let result = firstNumber - secondNumber 
-    //     console.log('this is the result', result);
-    // }
-    // function multiplyFunction(firstNumber, secondNumber)  {
-    //     console.log('this is the add function');
-    //     let result = firstNumber * secondNumber 
-    //     console.log('this is the result', result);
-    // }
-    // function divideFunction(firstNumber, secondNumber)  {
-    //     console.log('this is the add function');
-    //     let result = firstNumber / secondNumber 
-    //     console.log('this is the result', result);
-    // }
+function subtractFunction(firstNumber, secondNumber)  {
+        console.log('this is the add function');
+        let result = firstNumber - secondNumber 
+        console.log('this is the subtractFunction result', result);
+        return result;
+    }
+    function multiplyFunction(firstNumber, secondNumber)  {
+        console.log('this is the add function');
+        let result = firstNumber * secondNumber 
+        console.log('this is the multiplyFunction result', result);
+        return result;
+    }
+    function divideFunction(firstNumber, secondNumber)  {
+        console.log('this is the divide function');
+        let result = firstNumber / secondNumber 
+        console.log('this is the result', result);
+        return result;
+    }
 
 
 
-
+    doMath();
 
